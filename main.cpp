51d50d034685546a1d44cb018b156c7d1768ebb4
luciana1faur <luciana_faur@yahoo.com>
2020-09-22 07:47:14 +0000

    Conversie litera mare in litera mica

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-using namespace std;
 #include <cmath>
+#include <cctype>
+using namespace std;
 
 int main() {
   char rasp;
   double x;
 
-  rasp = 'd';
-  while (rasp == 'd') {
+  do {
     cout << "Introduceti gradele (x): ";
     cin >> x;
     cout << "sin(x) = " << sin(3.14159 * x / 180.) << endl;
     cout << "Mai continuati? (d/n) ";
     cin >> rasp;
   }
-  
+  while (tolower(rasp) == 'd'); 
+  //sau while (toupper(rasp) == 'D'); ... este acelasi lucru
 
   return 0;
 }
